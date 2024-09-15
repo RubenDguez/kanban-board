77645710838dd7ab3295ce6d497bc8ed98cb6342
Ruben Dominguez <71775213+RubenDguez@users.noreply.github.com>
2024-09-15 15:40:29 -0400

    Hotfix/cannot get login error (#1)
    * fix: Cannot GET /login
    
    * fix: Cannot GET /login

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,15 +1,21 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useCallback, useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import auth from '../utils/auth';
 
 const Navbar = () => {
   const [ loginCheck, setLoginCheck ] = useState(false);
+  const navigate = useNavigate();
 
   const checkLogin = () => {
     if(auth.loggedIn()) {
       setLoginCheck(true);
     }
   };
+
+  const handleLogout = useCallback(() => {
+    auth.logout();
+    navigate('/login');
+  }, [navigate]);
 
   useEffect(() => {
     checkLogin();
@@ -30,9 +36,7 @@ const Navbar = () => {
           </li>
         ) : (
           <li className='nav-item'>
-            <button type='button' onClick={() => {
-              auth.logout();
-            }}>Logout</button>
+            <button type='button' onClick={handleLogout}>Logout</button>
           </li>
         )
       }
