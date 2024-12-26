fafbe1acec754ca5713608082aaeda3fc45e8cfa
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-12-26 15:08:14 -0500

    update: script sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo yes | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 
@@ -13,8 +13,8 @@ node --version
 git --version
 
 npm install
-sudo yes | cp -rf kanban.service /etc/systemd/system/kanban.service
-sudo yes | cp -rf Caddyfile /etc/caddy/Caddyfile
+sudo yes | cp -f kanban.service /etc/systemd/system/kanban.service
+sudo yes | cp -f Caddyfile /etc/caddy/Caddyfile
 
 sudo systemctl daemon-reload
 sudo systemctl start kanban.service
