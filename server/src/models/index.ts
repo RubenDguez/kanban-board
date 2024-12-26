69f5664ea03023779ea1ac273e3c9fda51807181
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-12-26 13:26:42 -0500

    update: sequilize init

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -9,9 +9,14 @@ const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
       host: 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432'),
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       },
     });
 
@@ -19,6 +24,6 @@ const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 
 User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
-Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
+Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser' });
 
 export { sequelize, User, Ticket };
