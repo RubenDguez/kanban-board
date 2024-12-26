c08f651bdd06cbdb85dabddbba8519e8ff7ba6ea
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-12-26 15:57:08 -0500

    update: script sh

--- script.sh
+++ script.sh
@@ -6,15 +6,18 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+
+echo "\nInstalling Node JS"
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+sudo apt install -y nodejs
+echo "Node Version: $(node --version)"
+
+echo "\nInstalling Caddy"
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --yes --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
-
-sudo apt install -y nodejs
 sudo apt install caddy
 
-echo "Node Version: $(node --version)"
-echo "Git Version:  $(git --version)"
+echo "\nGit Version:  $(git --version)"
 
 npm install
 sudo mv -f kanban.service /etc/systemd/system/kanban.service
