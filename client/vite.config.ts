eb8ca46a77d7e64f9c5fd79dc55afe429fcede0c
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-12-26 14:24:04 -0500

    update: client vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 3000,
-    open: true,
+    open: false,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
