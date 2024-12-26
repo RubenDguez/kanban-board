aade620d3b07af17bd80e3248ca5a4b7bd167219
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-12-26 14:53:16 -0500

    add: aws ec2 deployment files

---
+++ script.sh
@@ -0,0 +1,25 @@
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+
+sudo apt install -y nodejs
+sudo apt install caddy
+
+node --version
+git --version
+git clone https://github.com/RubenDguez/kanban-board.git
+cd kanban-board
+npm install
+sudo cp kanban.service /etc/systemd/system/kanban.service
+
+sudo systemctl daemon-reload
+sudo systemctl start kanban.service
+sudo systemctl enable kanban.service
+
+sudo cp Caddyfile > /etc/caddy/Caddyfile
+
+sudo systemctl restart caddy
